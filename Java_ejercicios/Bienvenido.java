deb709de3923f04339e1e9f75eca5f9f43996a31
davidperlaza14 <davidperlaza14@gmail.com>
2023-04-06 06:17:04 -0500

    1000 Ejercicios con Java

---
+++ Java_ejercicios/Bienvenido.java
@@ -0,0 +1,13 @@
+package Java_ejercicios;
+/*
+ * Clase de bienvenida.
+ * @author David Stiven
+ * @version 11.0.18
+ */
+public class Bienvenido {
+
+    public static void main(String[] args) {
+            System.out.println("Bienvenido a JAVA");
+
+    }
+}
